006faf9099ff01c93738eb397633314e97620b16
Nutan Singh <nutankindtech.co.in@Nutans-MacBook-Air.local>
2025-11-13 15:09:01 +0530

    config files

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const authToken = request.cookies.get("authToken")?.value;
+  const loggedin = !!authToken;
+
+  const loginUrl = new URL("/login", request.url);
+  const quizUrl = new URL("/quiz", request.url);
+  const requestedPage = request.nextUrl.pathname;
+
+  if (loggedin && (requestedPage === "/" || requestedPage === "/login"))
+    return NextResponse.redirect(quizUrl);
+  if (!loggedin && requestedPage.startsWith("/quiz"))
+    return NextResponse.redirect(loginUrl);
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/quiz/:path*", "/login"],
+};
